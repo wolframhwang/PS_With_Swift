ba1b72eec3730290703cce2071dc32a16eb1d3e0
Jiwoong Hwang <hzw94@naver.com>
2022-09-01 17:54:19 +0900

    Create Same Tree.swift

---
+++ volga/220901/Same Tree.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil, q == nil { return true }
+        if p == nil, q != nil { return false }
+        if p != nil, q == nil { return false }
+        if p?.val != q?.val { return false }
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+}
