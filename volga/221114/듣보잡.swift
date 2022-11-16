067d51ee053449177eae5297fbe9f3e81bc29098
Jiwoong Hwang <hzw94@naver.com>
2022-11-16 21:15:19 +0900

    Create 듣보잡.swift

---
+++ volga/221114/듣보잡.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let file = FileIO()
+
+let N = file.readInt(), M = file.readInt()
+var dict: [String: Int] = [:]
+var answer: [String] = []
+for _ in 0..<N {
+    let str = file.readString()
+    dict[str, default: 0] += 1
+}
+
+for _ in 0..<M {
+    let str = file.readString()
+    dict[str, default: 0] += 1
+    if dict[str, default: 0] > 1 { answer.append(str) }
+}
+print(answer.count)
+answer.sorted { $0 < $1 }.forEach { str in
+    print(str)
+}
